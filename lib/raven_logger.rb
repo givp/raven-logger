e212e3d4cb8308fd695cbcd1b376b065e9477bc4
Giv Parvaneh <giv@givp.org>
2018-08-02 19:24:47 -0700

    First version

--- lib/raven_logger.rb
+++ lib/raven_logger.rb
@@ -1,6 +1,10 @@
 require 'securerandom'
 require 'digest'
 require 'socket'
+require 'time'
+require 'rest-client'
+require 'json'
+require 'git-revision'
 
 class RavenLogger
 
@@ -14,7 +18,7 @@ class RavenLogger
   def captureMessage(title, opts={})
     raise ArgumentError.new('Event title is missing') if !title
 
-    opts.symbolize_keys!
+    opts = symbolize_keys(opts)
     opts[:level] ||= :info
     extra = opts[:extra] || {}
     level = opts[:level].to_s
@@ -51,6 +55,40 @@ class RavenLogger
       "Content-Type": "application/json",
       "X-Sentry-Auth": "Sentry sentry_version=7, sentry_timestamp=#{Time.now.utc.to_i}, sentry_key=#{sentry_key}, sentry_secret=#{sentry_secret}, sentry_client=raven-intricately/1.0"
     }
+  end
+
+  def symbolize_keys(obj)
+    case obj
+    when Array
+      obj.inject([]){|res, val|
+        res << case val
+        when Hash, Array
+          symbolize_keys(val)
+        else
+          val
+        end
+        res
+      }
+    when Hash
+      obj.inject({}){|res, (key, val)|
+        nkey = case key
+        when String
+          key.to_sym
+        else
+          key
+        end
+        nval = case val
+        when Hash, Array
+          symbolize_keys(val)
+        else
+          val
+        end
+        res[nkey] = nval
+        res
+      }
+    else
+      obj
+    end
   end
 
 end
